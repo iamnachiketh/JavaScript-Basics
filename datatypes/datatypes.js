9443a8a3066b8368720620c5d5b9b7d47239789b
Nachiketha <nachiketh.k.c@brainsightai.com>
2024-12-11 21:11:31 +0530

    added datatypes to the master branch

---
+++ datatypes/datatypes.js
@@ -0,0 +1,103 @@
+//Primitive
+//Number
+let a = 10;
+let b = 2.78;
+let y = 123e-5;
+
+console.log(a," ",b);
+console.log(typeof a);
+console.log(typeof y);
+
+
+console.log("===============================================");
+
+//String
+let c = "Hello";
+let d = "This is me!!";
+console.log(c," ",d);
+console.log(typeof c);
+
+console.log("===============================================");
+
+
+//BigInt
+let e = 454378956432456n;
+console.log(e);
+console.log(typeof e);
+
+console.log("===============================================");
+
+
+//Boolean
+let f = true;
+console.log(f);
+console.log(typeof f);
+
+console.log("===============================================");
+
+
+//Undefined
+let h = undefined;
+console.log(h);
+console.log(typeof h);
+
+console.log("===============================================");
+
+//Symbole
+let sym = Symbol("hello");
+console.log(sym);
+console.log(typeof sym);
+
+let sin = new String("hello");
+console.log(sin);
+
+console.log("===============================================");
+
+//Non - Primitive
+// Note: All the types of Wrapper Object are Object type.
+//Object
+let g = null;
+console.log(g);
+console.log(typeof g);
+
+console.log("===============================================");
+
+//Array Object
+let arr = [];
+let arr2 = [10,"hello",23.987,true,{name:"arr2"}];
+console.log(arr);
+console.log(arr2);
+console.log(typeof arr);
+
+console.log("===============================================");
+
+//Object
+let i = {name:"alex",age:20,city:"Banglore"};
+console.log(i);
+console.log(typeof i);
+
+console.log("===============================================");
+
+let numb = new Number(10);
+console.log(numb);
+console.log(typeof numb);
+
+console.log("===============================================");
+
+let str = new String(10);
+console.log(str);// '10'
+console.log(typeof str);//Object
+
+console.log("===============================================");
+
+let bool = new Boolean(10);
+console.log(bool);// true
+console.log(typeof bool); // Object
+
+
+
+
+
+
+
+
